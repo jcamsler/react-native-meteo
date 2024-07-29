93b5c4081f7faa03e6e2a956401e9ad8580713ba
Jc AMSLER <jcamsler@gmail.com>
2024-07-29 16:56:48 +0200

    Ajout des donnees EAS d expo (build dev prod)

--- App.js
+++ App.js
@@ -4,6 +4,13 @@ import AlataRegular from "./assets/fonts/Alata-Regular.ttf";
 import { useFonts } from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useEffect, useRef } from "react";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { Subscription } from "expo-notifications";
+import Constants from "expo-constants";
+import { Platform } from "react-native";
+
 const Stack = createNativeStackNavigator();
 
 const navTheme = {
@@ -13,9 +20,80 @@ const navTheme = {
 };
 
 export default function App() {
+  const notificationListener = useRef(Notifications.Subscription);
+  const responseListener = useRef(Notifications.Subscription);
+
   const [isFontLoaded] = useFonts({
     "Alata-Regular": AlataRegular,
   });
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
+
+  async function subscribeToNotifications() {
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        handleRegistrationError(
+          "Permission not granted to get push token for push notification!"
+        );
+        return;
+      }
+      const projectId =
+        Constants?.expoConfig?.extra?.eas?.projectId ??
+        Constants?.easConfig?.projectId;
+      if (!projectId) {
+        handleRegistrationError("Project ID not found");
+      }
+      try {
+        const pushTokenString = (
+          await Notifications.getExpoPushTokenAsync({
+            projectId,
+          })
+        ).data;
+        console.log(pushTokenString);
+        return pushTokenString;
+      } catch (e) {
+        handleRegistrationError(`${e}`);
+      }
+    } else {
+      handleRegistrationError(
+        "Must use physical device for push notifications"
+      );
+    }
+  }
+
+  function handleRegistrationError(errorMessage) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
+
+  useEffect(() => {
+    Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log("data", response.notification.request.content.data);
+    });
+    subscribeToNotifications();
+  }, []);
 
   console.log("isFontLoaded:" + isFontLoaded);
 
